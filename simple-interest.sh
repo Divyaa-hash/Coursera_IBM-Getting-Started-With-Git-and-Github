88082204bb617d2ed0e9fa2a0ff1e1abe5686db1
Divyaa-hash <divyaalwar12@gmail.com>
2024-10-11 12:51:33 +0530

    Bug-fixed-typo

--- simple-interest.sh
+++ simple-interest.sh
@@ -11,7 +11,7 @@ echo "Enter the time period (in years):"
 read time
 
 # Calculate simple interest
-intarest=$(echo "$principal * $rate * $time" | bc)
+interest=$(echo "$principal * $rate * $time" | bc)
 
 # Display the result
 echo "Simple Interest: $interest"
